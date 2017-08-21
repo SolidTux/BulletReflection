aea575e769a0cba767c5deca59615dccdd033d6b
Daniel Hauck <mail@dhauck.eu>
2017-08-21 21:21:21 +0200

    Implement basic game logic.

--- core/src/de/dhermit/bulletreflection/BulletReflection.java
+++ core/src/de/dhermit/bulletreflection/BulletReflection.java
@@ -22,7 +22,8 @@ public class BulletReflection extends ApplicationAdapter implements InputProcess
     OrthogonalTiledMapRenderer mapRenderer;
     OrthographicCamera camera;
     int mapWidth, mapHeight;
-    Vector2 dir;
+    TiledMapTileLayer walls, background;
+    Vector2 dir, newPosition;
 
     @Override
     public void create () {
@@ -31,12 +32,15 @@ public class BulletReflection extends ApplicationAdapter implements InputProcess
         camera = new OrthographicCamera(10, 10);
         mapWidth = map.getProperties().get("width",Integer.class);
         mapHeight = map.getProperties().get("height",Integer.class);
+        walls = (TiledMapTileLayer) map.getLayers().get("walls");
+        background = (TiledMapTileLayer) map.getLayers().get("background");
 
         batch = new SpriteBatch();
         player = new Sprite(new Texture(Gdx.files.internal("player.png")));
         player.setSize(1, 1);
 
         dir = new Vector2();
+        newPosition = new Vector2();
         shapeRenderer = new ShapeRenderer();
 
         Gdx.input.setInputProcessor(this);
@@ -74,9 +78,79 @@ public class BulletReflection extends ApplicationAdapter implements InputProcess
                 break;
             case Input.Keys.ENTER:
                 dir = new Vector2();
+                player.setPosition(newPosition.x, newPosition.y);
+                newPosition = new Vector2();
+                break;
+            case Input.Keys.ESCAPE:
+                dir = new Vector2();
+                player.setPosition(0, 0);
                 break;
         }
         return false;
+    }
+
+    private Vector2 bounceBullet(Vector2 start, Vector2 dir, int num) {
+        Vector2 direction = new Vector2(dir);
+        direction.nor();
+        Vector2 startPosition = new Vector2(start);
+        Vector2 position = new Vector2(start);
+        TiledMapTileLayer.Cell cell = walls.getCell((int) position.x, (int) position.y);
+        int reflCount = 0;
+        int cellId = 1;
+        boolean reflected = false;
+
+        if (cell != null) {
+            cellId = walls.getCell((int) position.x, (int) position.y).getTile().getId();
+        }
+        while (reflCount < num) {
+            position.add(direction);
+            if (position.x < 0) {
+                direction.x = -direction.x;
+                position.x = 0;
+                reflected = true;
+            }
+            if (position.x > mapWidth) {
+                direction.x = -direction.x;
+                position.x = mapWidth - 1;
+                reflected = true;
+            }
+            if (position.y < 0) {
+                direction.y = -direction.y;
+                position.y = 0;
+                reflected = true;
+            }
+            if (position.y > mapHeight) {
+                direction.y = -direction.y;
+                position.y = mapHeight - 1;
+                reflected = true;
+            }
+            cell = walls.getCell((int) position.x, (int) position.y);
+            if (cell != null) {
+                cellId = walls.getCell((int) position.x, (int) position.y).getTile().getId();
+            } else {
+                cellId = 1;
+            }
+            switch (cellId) {
+                case 3:
+                    direction.rotate90(1);
+                    direction.y = -direction.y;
+                    reflected = true;
+                    break;
+                case 4:
+                    direction.rotate90(1);
+                    direction.x = -direction.x;
+                    reflected = true;
+                    break;
+            }
+            if (reflected) {
+                reflected = false;
+                reflCount++;
+                shapeRenderer.line(startPosition.x + 0.5f, startPosition.y + 0.5f,
+                    position.x + 0.5f, position.y + 0.5f);
+                startPosition = new Vector2(position);
+            }
+        }
+        return position;
     }
 
     @Override
@@ -91,17 +165,12 @@ public class BulletReflection extends ApplicationAdapter implements InputProcess
         mapRenderer.render();
 
         // Draw path.
-        if (dir.len() != 0) {
+        if (!dir.isZero()) {
             shapeRenderer.setProjectionMatrix(camera.combined);
-            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
             shapeRenderer.setColor(1, 1, 0, 1);
             Vector2 playerPos = new Vector2(player.getX(), player.getY());
-            playerPos.add(0.5f, 0.5f);
-            Vector2 endPos = new Vector2(dir);
-            endPos.scl(3);
-            System.out.println(endPos);
-            endPos.add(playerPos);
-            shapeRenderer.line(playerPos.x, playerPos.y, endPos.x, endPos.y);
+            newPosition = bounceBullet(playerPos, dir, 5);
             shapeRenderer.end();
         }
 
@@ -110,6 +179,15 @@ public class BulletReflection extends ApplicationAdapter implements InputProcess
         batch.begin();
         player.draw(batch);
         batch.end();
+
+        // Test if game is won.
+        TiledMapTileLayer.Cell current = background.getCell((int) player.getX(), (int) player.getY());
+        if (current != null) {
+            System.out.println(current.getTile().getId());
+            if (current.getTile().getId() == 6) {
+                Gdx.app.exit();
+            }
+        }
     }
 
     @Override
